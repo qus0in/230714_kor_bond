25fe0696d97e3b84266f11c8b8c6561a3dd3a2fe
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-14 17:30:19 +0900

    date.now

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,26 @@
 import streamlit as st
+import pandas as pd
+import requests
+from datetime import date
+@st.cache_data
+def get_bond_info(basDt):
+    url = 'http://apis.data.go.kr/1160100/service/GetBondIssuInfoService/getBondBasiInfo'
+    params = lambda x: dict(
+        servicKey = st.secrets['BOND_INFO_SK'],
+        numOfRows = 10000,
+        pageNo = x,
+        resultType = 'json',
+        basDt = basDt
+    )
+    w = 1
+    data = []
+    while True:
+        response = requests.get(url, params=params(w))
+        df = pd.DataFrame(response.json()['response']['body']['items']['item'])
+        if not len(df): break
+        data.append(df)
+        w += 1
+    return pd.concat(data)
 
 st.write("hello!")
-st.write(st.secrets['BOND_INFO_SK'])
+st.write(date.now())
